b6e1590fc93a058df97ccba17ea956e23689e93f
Andrey <andrey@MacBook-Air-Andrey.local>
2022-06-11 16:46:40 +0300

    add loader.js

--- src/JS/theme.js
+++ src/JS/theme.js
@@ -1,5 +1,27 @@
 const themeBtn = document.getElementById('toggle-theme-btn')
+const themeImg = document.getElementById('toggle-theme-img')
 
 themeBtn.addEventListener('click', () => {
-  document.body.classList.toggle('dark')
+  if (document.body.classList.contains('dark')) {
+    setLightTheme()
+  } else {
+    setDarkTheme()
+  }
 })
+
+function setDarkTheme() {
+  document.body.classList.add('dark')
+  themeImg.src = 'images/theme/sunnysunshine.ico'
+  localStorage.theme = 'dark'
+}
+
+function setLightTheme() {
+  document.body.classList.remove('dark')
+  themeImg.src = 'images/theme/moon.png'
+  localStorage.theme = 'light'
+}
+
+if (localStorage.theme === 'dark') {
+  setDarkTheme()
+}
+console.log(themeImg.src);
--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 import './js/getMovies';
 import './js/insertMovies';
 import './JS/theme';
+import './JS/loader';
