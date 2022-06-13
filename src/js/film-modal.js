4586f261efd58d5de808ea39046f61aa8c4cd7e1
Vlad Koblay <vkchabraww@gmail.com>
2022-06-13 21:49:16 +0300

    adds film to localStorage
    поки що все в консоль лозі, не хочу сильно затягувати щоб не було конфліктів потім але не можу розібратися з цим просто зараз
    Плюс мінуснув консоль логи які були до цього в коментарі

--- src/js/film-modal.js
+++ src/js/film-modal.js
@@ -2,6 +2,8 @@ import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 import { getMoviesById } from './get-movies';
 import { startLoader, stopLoader } from './loader';
+import { addFilmToWatched } from './film-local-storage';
+import { addFilmToQueued } from './film-local-storage';
 
 const filmsContainer = document.querySelector('.cards-collection');
 let instance;
@@ -16,9 +18,16 @@ function handleCardClick(event) {
   putFetchToMarkup(filmId).then(markup => {
     instance = basicLightbox.create(markup, {
       onShow: () => {
+        // const modalBtnWatched = document.querySelector('.modal-btn-watched');
+        // console.log(modalBtnWatched);
+        // console.log(filmId);
+        // modalBtnWatched.addEventListener('click', addFilmToWatched(filmId))
+        // modalBtnQueued.addEventListener('click', addFilmToQueued(filmId))
         window.addEventListener('keydown', onEskPress);
       },
       onClose: () => {
+        // modalBtnWatched.removeEventListener('click', addFilmToWatched(filmId))
+        // modalBtnQueued.removeEventListener('click', addFilmToQueued(filmId))
         window.removeEventListener('keydown', onEskPress);
       },
     });
@@ -58,50 +67,51 @@ function makeFilmModalMarkup(result) {
         original_title,
         genre_ids,
         overview,
-      }) =>
+      }) => {
+        return `
+        <div class="film-modal" data-id=${id}>
+        <div class="film-poster-wrapper">
+          <img class="film-poster-img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}"/>
+        </div>
+        <div class="film-info-wrapper">
+          <h2 class="film-modal-header">${title}</h2>
+          <ul class="film-modal-info">
+            <li class="film-modal-item">
+              <p class="film-item-title">Vote / Votes</p>
+              <p class="film-item-desc">
+                <span class="vote-mod color">${vote_average}</span>
+                / <span class="vote-mod">${vote_count}</span>
+              </p>
+            </li>
+            <li class="film-modal-item">
+              <p class="film-item-title">Popularity</p>
+              <p class="film-item-desc">${popularity}</p>
+            </li>
+            <li class="film-modal-item">
+              <p class="film-item-title">Original Title</p>
+              <p class="film-item-desc original-title">${original_title}</p>
+            </li>
+            <li class="film-modal-item">
+              <p class="film-item-title">Genre</p>
+              <p class="film-item-desc genre-desc">${genre_ids}</p>
+            </li>
+          </ul>
+          <h3 class="film-modal-about original-title">ABOUT</h3>
+          <p class="film-modal-overview">${overview}</p>
+          <div class="film-modal-btns">
+            <button class="modal-btn-watched" type="button">add to Watched</button>
+            <button class="modal-btn-queue" type="button">add to queue</button>
+          </div>
+      </div>    
+          <button class="film-modal-close" type="button">
+            <svg class="modal-close-ico" width="30" height="30" viewBox="0 0 30 30">
+              <path class="modal-close-ico" d="M8 8L22 22" stroke="black" stroke-width="2"></path>
+              <path class="modal-close-ico" d="M8 22L22 8" stroke="black" stroke-width="2"></path>
+            </svg>
+          </button>
+        </div>
         `
-  <div class="film-modal" data-id=${id}>
-  <div class="film-poster-wrapper">
-    <img class="film-poster-img" src="https://image.tmdb.org/t/p/w500/${poster_path}" alt="${title}"/>
-  </div>
-  <div class="film-info-wrapper">
-    <h2 class="film-modal-header">${title}</h2>
-    <ul class="film-modal-info">
-      <li class="film-modal-item">
-        <p class="film-item-title">Vote / Votes</p>
-        <p class="film-item-desc">
-          <span class="vote-mod color">${vote_average}</span>
-          / <span class="vote-mod">${vote_count}</span>
-        </p>
-      </li>
-      <li class="film-modal-item">
-        <p class="film-item-title">Popularity</p>
-        <p class="film-item-desc">${popularity}</p>
-      </li>
-      <li class="film-modal-item">
-        <p class="film-item-title">Original Title</p>
-        <p class="film-item-desc original-title">${original_title}</p>
-      </li>
-      <li class="film-modal-item">
-        <p class="film-item-title">Genre</p>
-        <p class="film-item-desc genre-desc">${genre_ids}</p>
-      </li>
-    </ul>
-    <h3 class="film-modal-about original-title">ABOUT</h3>
-    <p class="film-modal-overview">${overview}</p>
-    <div class="film-modal-btns">
-      <button class="modal-btn-watched" type="button">add to Watched</button>
-      <button class="modal-btn-queue" type="button">add to queue</button>
-    </div>
-</div>    
-    <button class="film-modal-close" type="button">
-      <svg class="modal-close-ico" width="30" height="30" viewBox="0 0 30 30">
-        <path class="modal-close-ico" d="M8 8L22 22" stroke="black" stroke-width="2"></path>
-        <path class="modal-close-ico" d="M8 22L22 8" stroke="black" stroke-width="2"></path>
-      </svg>
-    </button>
-  </div>
-  `,
+      },
     )
     .join('');
 
