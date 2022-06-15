a204d50c0a28aba6e8be4d314c4ffba99e43ddaa
Minaiev Kiril <kiril777kiril@gmail.com>
2022-06-15 10:20:30 +0300

    bug fix

--- src/js/header-mylib.js
+++ src/js/header-mylib.js
@@ -2,7 +2,8 @@ const header = document.querySelector('.header'); // хедер
 const form = document.querySelector('.input__wrapper'); // форма хедеру
 const headerHome = document.querySelector('.home__btn'); // кнопка навігації Home
 const headerLibrary = document.querySelector('.my-library__btn'); // кнопка навігації My library
-const myLibraryBtnList = document.querySelector('.library-btn__list'); // кнопки хедеру My library Watched та Queue
+const myLibraryBtnList = document.querySelector('.library-btn__list');
+const logo = document.querySelector('.logo__list'); // кнопки хедеру My library Watched та Queue
 
 headerHome.addEventListener('click', onheaderHomeBtnClick);
 headerLibrary.addEventListener('click', onMyLibraryBtnClick);
