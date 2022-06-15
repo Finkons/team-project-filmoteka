038212f39fe928b1a6915963e55ae438d8392d4a
Vlad Koblay <vkchabraww@gmail.com>
2022-06-15 18:00:03 +0300

    local-storage toggle
    0 конфліктів, самий актуальний local-storage update

--- src/js/film-local-storage.js
+++ src/js/film-local-storage.js
@@ -1,7 +1,24 @@
+const queueStoragedFilms = [];
+const watchedStoragedFilms = [];
+
 export function addFilmToQueued (filmId) {
-    localStorage.setItem("queued films", filmId)
+    localStorage.setItem("queued_films", queueStoragedFilms)
+
+    if (localStorage.queued_films.includes(filmId)) {
+        queueStoragedFilms.splice(queueStoragedFilms.indexOf(filmId), 1)
+    } else {
+        queueStoragedFilms.push(filmId);
+    }
+    localStorage.setItem("queued_films", queueStoragedFilms)
 }
 
 export function addFilmToWatched (filmId) {
-    localStorage.setItem("watched films", filmId)
+    localStorage.setItem("watched_films", watchedStoragedFilms)
+
+    if (localStorage.watched_films.includes(filmId)) {
+        watchedStoragedFilms.splice(watchedStoragedFilms.indexOf(filmId), 1)
+    } else {
+        watchedStoragedFilms.push(filmId);
+    }
+    localStorage.setItem("watched_films", watchedStoragedFilms)
 }
