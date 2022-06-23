9f042f14639f3b1603ccdc4602429159320c568d
kateryna klimchuk <ketyclima@gmail.com>
2022-06-23 13:16:35 +0300

    fixed searching at the search form

--- src/js/search-form.js
+++ src/js/search-form.js
@@ -1,4 +1,7 @@
 import { getGenres, API_KEY } from './get-movies';
+import refs from './refs';
+import { otherGenresTemplate } from './render-local-movies';
+
 import { startLoader, stopLoader } from './loader.js';
 import moviesListPatern from '../templates/list-of-movies.hbs';
 import Notiflix from 'notiflix';
@@ -6,26 +9,28 @@ import axios from 'axios';
 import { langCurrent } from './language';
 import { renderButtons } from './pagination';
 
+export const SEARCH_TYPE = 'current-search-fetch';
 
 const BASE_URL = "https://api.themoviedb.org/3/discover/movie?";
-
-const galleryEl = document.querySelector('.cards-collection');
 const formEl = document.querySelector('.genre-search');
 const searchGenreEl = document.querySelector('#genres');
 const searchBtnOpen = document.querySelector('.search-btn--open');
 const searchBtnClose = document.querySelector('.search-btn--close');
 const searchBackdrop = document.querySelector('.search-form__wrap');
 
+formEl.addEventListener('change', onSearchFormChange);
+searchBtnOpen.addEventListener('click', onClickSearchBtnOpen);
+searchBtnClose.addEventListener('click', onClickSearchBtnClose);
+
+const currentLang = localStorage.getItem('lang');
+
 renderGenresList();
 
 let genresList;
-export const SEARCH_TYPE = 'current-search-fetch';
 
-
-formEl.addEventListener('change', (event) => {
+function onSearchFormChange(event) {
     const formValue = event.target;
     event.preventDefault();
-
 if (formValue.id === 'years') {
 
     if (formValue.value !== 'year') {
@@ -33,25 +38,19 @@ if (formValue.id === 'years') {
         onClickSearchBtnClose();
         Notiflix.Notify.success(`Hooray! Here your films by ${formValue.value} year!`);
         clearGallery();
-        insertGenresToMoviesByYear(formValue.value);
+        insertGenresToMoviesByYear(formValue.value, currentLang);
         localStorage.setItem(SEARCH_TYPE, "byYear");
-
         stopLoader();
     }}
-
-    
     if (formValue.id === 'genres') {
-
         let genreId;
-
         if (formValue.value !== 'genres') {
             startLoader();
             Notiflix.Notify.success(`Hooray! Here your ${formValue.value} movies!`);
             for (const el of genresList) {
-
                 if (el.name === formValue.value) {
                     genreId = el.id;
-                    insertGenresToMoviesByGenres(genreId);
+                    insertGenresToMoviesByGenres(genreId, currentLang);
                     localStorage.setItem(SEARCH_TYPE, "byGenres");
                     onClickSearchBtnClose();
                 }
@@ -60,24 +59,10 @@ if (formValue.id === 'years') {
             stopLoader()
         }
     }
-
-    // if (formValue.id === 'popularity') {
-
-    //     if (formValue.value !== 'option') {
-    //         startLoader();
-    //         onClickSearchBtnClose();
-    //         Notiflix.Notify.success(`Hooray! We found most popular movies!`);
-    //         clearGallery();
-    //         markupMoviesByPopularity(formValue.value);
-    //         stopLoader();
-    //     }
-    // }
     formEl.reset();
-})
+}
 
 
-searchBtnOpen.addEventListener('click', onClickSearchBtnOpen)
-searchBtnClose.addEventListener('click', onClickSearchBtnClose)
 function onClickSearchBtnOpen() {
     searchBackdrop.classList.add('is-open');
 }
@@ -88,12 +73,17 @@ function onClickSearchBtnClose() {
 
 
 function clearGallery() {
-    galleryEl.innerHTML = '';
+    refs.galleryContainer.innerHTML = '';
 }
 
-async function renderGenresList() {
-
-    const response = await getGenres(langCurrent());
+async function renderGenresList(lang) {
+    let pageLang = localStorage.getItem('lang');
+    if (pageLang === 'ua') {
+    lang = `uk`;
+    } else {
+    lang = `en`;
+    }
+    const response = await getGenres(lang);
     genresList = response.genres;
     const genresItems = genresList.map(({ name }) => {
     return `<option value="${name}">${name}</option>`
@@ -102,170 +92,44 @@ searchGenreEl.insertAdjacentHTML('beforeend', genresItems)
 }
 
 
-// async function getMoviesByGenres(genreId) {
-//     const url = `${BASE_URL}api_key=${API_KEY}&language=en-US&include_adult=false&include_video=false&page=1&with_genres=${genreId}`;
-//     const response = await axios.get(url);
-//     return response.data.results;
-// };
-
-// async function getMoviesByYear(year) {
-//     const url = `${BASE_URL}api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=${year}-01-01&primary_release_date.lte=${year}-12-31`;
-//     const response = await axios.get(url);
-//     return response.data.results;
-// }
-
-// async function getMoviesByPopularity(param) {
-//     const url = `${BASE_URL}api_key=${API_KEY}&language=en-US&sort_by=${param}.desc&include_adult=false`;
-//     const response = await axios.get(url);
-//     return response.data.results;
-// }
-
-
-// async function insertGenresToMoviesByPopularity(param) {
-//     const data = await getMoviesByPopularity(param);
-//     const genresList = await getGenres(langCurrent());
-//     return data.map(movie => ({
-//         ...movie,
-//         release_date: movie.release_date.split('-')[0],
-//         genres: movie.genre_ids
-//             .map(id => genresList.genres.filter(el => el.id === id))
-//             .flat(),
-//     }));
-// }
-
-// function markupMoviesByPopularity(param) {
-//     insertGenresToMoviesByPopularity(param).then(res => {
-//     res.map(element => {
-//     if (element.genres.length > 2) {
-//         const Obj = {name: "Other"};
-//         element.genres[2] = Obj;
-//         element.genres.length = 3
-//     }
-//     })
-//         renderMoviesList(res);
-// }).catch(error => {
-//     console.log(error.message)
-// })
-// }
-
-
-// async function insertGenresToMoviesByGenres(id) {
-//     const data = await getMoviesByGenres(id);
-//     const genresList = await getGenres(langCurrent());
-//     return data.map(movie => ({
-//         ...movie,
-//         release_date: movie.release_date.split('-')[0],
-//         genres: movie.genre_ids
-//             .map(id_1 => genresList.genres.filter(el => el.id === id_1))
-//             .flat(),
-//     }));
-// }
-
-// function markupMoviesByGenres(id) {
-//     insertGenresToMoviesByGenres(id).then(res => {
-//         res.map(element => {
-//     if (element.genres.length > 2) {
-//         const Obj = {name: "Other"};
-//         element.genres[2] = Obj;
-//         element.genres.length = 3
-//     }
-//     })
-//         return renderMoviesList(res);
-// }).catch(error => {
-//     console.log(error.message)
-// })
-// }
-
-// async function insertGenresToMoviesByYear(year) {
-//     const data = await getMoviesByYear(year);
-//     const genresList = await getGenres(langCurrent());
-//     return data.map(movie => ({
-//         ...movie,
-//         release_date: movie.release_date.split('-')[0],
-//         genres: movie.genre_ids
-//             .map(id => genresList.genres.filter(el => el.id === id))
-//             .flat(),
-//     }));
-// }
-
-// function markupMoviesByYear(year) {
-//     insertGenresToMoviesByYear(year).then(res => {
-//     res.map(element => {
-//     if (element.genres.length > 2) {
-//         const Obj = {name: "Other"};
-//         element.genres[2] = Obj;
-//         element.genres.length = 3
-//     }
-//     })
-//         renderMoviesList(res);
-// }).catch(error => {
-//     console.log(error.message)
-// })
-// }
-
 function renderMoviesList(movies) {
     const markup = moviesListPatern(movies)
-    galleryEl.insertAdjacentHTML('beforeend', markup);
+    refs.galleryContainer.insertAdjacentHTML('beforeend', markup);
 }
 
 
-async function getMoviesByGenres(genreId, page = 1) {
-    const url = `${BASE_URL}api_key=${API_KEY}&language=en-US&include_adult=false&include_video=false&page=${page}&with_genres=${genreId}`;
+async function getMoviesByGenres(genreId, page = 1, lang) {
+    let pageLang = localStorage.getItem('lang');
+    if (pageLang === 'ua') {
+    lang = `uk`;
+    } else {
+    lang = `en`;
+    }
+    const url = `${BASE_URL}api_key=${API_KEY}&language=${lang}&include_adult=false&include_video=false&page=${page}&with_genres=${genreId}`;
     const response = await axios.get(url);
     return response.data;
 };
 
-async function getMoviesByYear(year, page = 1) {
-    const url = `${BASE_URL}api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&primary_release_date.gte=${year}-01-01&primary_release_date.lte=${year}-12-31`;
+async function getMoviesByYear(year, page = 1, lang) {
+    let pageLang = localStorage.getItem('lang');
+    if (pageLang === 'ua') {
+    lang = `uk`;
+    } else {
+    lang = `en`;
+    }
+    const url = `${BASE_URL}api_key=${API_KEY}&language=${lang}&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&primary_release_date.gte=${year}-01-01&primary_release_date.lte=${year}-12-31`;
     const response = await axios.get(url);
     return response.data;
 }
 
-// async function getMoviesByPopularity( page = 1) {
-//     const url = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&include_adult=false&page=${page}`;
-//     const response = await axios.get(url);
-//     return response.data;
-// }
-
-
-// export async function insertGenresToMoviesByPopularity(page) {
-//     try {
-//         const fetchTrendMovie = await getMoviesByPopularity(page);
-//         const genresList = await getGenres(langCurrent());
-//         renderButtons(fetchTrendMovie.page, 500, query); //// для Вадима ;) // paginationFunction(currentPage, totalPages, searchQuery)
-
-//         const fetchRes = fetchTrendMovie.results.map(movie => ({
-//             ...movie,
-//             release_date: movie.release_date.split('-')[0],
-//             genres: movie.genre_ids
-//                 .map(id => genresList.genres.filter(el => el.id === id))
-//                 .flat(),
-//         }));
-//         markupMoviesByPopularity(fetchRes);
-//     }
-//     catch (error) {
-//         console.log(error.message);
-// }
-// }
-
-// function markupMoviesByPopularity(res) {
-//         res.map(element => {
-//             if (element.genres.length > 2) {
-//                 const Obj = { name: "Other" };
-//                 element.genres[2] = Obj;
-//                 element.genres.length = 3
-//             }
-//         })
-//         return renderMoviesList(res);
-//     }
-
 
 export async function insertGenresToMoviesByGenres(id, page) {
     try {
-        const fetchedGenres = await getMoviesByGenres(id, page);
+    const fetchedGenres = await getMoviesByGenres(id, page);
     clearGallery();
-    const genresList = await getGenres(langCurrent());
-    renderButtons(fetchedGenres.page, 500, id); //// для Вадима ;) // paginationFunction(currentPage, totalPages, searchQuery)
+        const genresList = await getGenres(langCurrent());
+        const totalPages = fetchedGenres.total_pages;
+        totalPages > 500 ? renderButtons(fetchedGenres.page, 500, id) : renderButtons(fetchedGenres.page, fetchedGenres.total_pages-1, id);
     const fetchRes = fetchedGenres.results.map(movie => ({
         ...movie,
         release_date: movie.release_date.split('-')[0],
@@ -273,22 +137,22 @@ export async function insertGenresToMoviesByGenres(id, page) {
             .map(id_1 => genresList.genres.filter(el => el.id === id_1))
             .flat(),
     }));
-    markupMoviesByGenres(fetchRes);
+    markupMoviesWithParams(fetchRes);
     }
     catch (error) {
         console.log(error);
     }
 }
 
-function markupMoviesByGenres(id) {
-        id.map(element => {
+function markupMoviesWithParams(searchParam) {
+        searchParam.map(element => {
     if (element.genres.length > 2) {
-        const Obj = {name: "Other"};
+        const Obj = otherGenresTemplate();
         element.genres[2] = Obj;
         element.genres.length = 3
     }
     })
-        return renderMoviesList(id);
+        return renderMoviesList(searchParam);
 }
 
 
@@ -297,7 +161,8 @@ export async function insertGenresToMoviesByYear(year, page) {
         const fetchedYear = await getMoviesByYear(year, page);
         clearGallery();
         const genresList = await getGenres(langCurrent());
-        renderButtons(fetchedYear.page, 500, year); //// для Вадима ;) // paginationFunction(currentPage, totalPages, searchQuery)
+        const totalPages = fetchedYear.total_pages;
+        totalPages > 500 ? renderButtons(fetchedYear.page, 500, year) : renderButtons(fetchedYear.page, fetchedYear.total_pages-1, year);
         const fetchRes = fetchedYear.results.map(movie => ({
             ...movie,
             release_date: movie.release_date.split('-')[0],
@@ -305,21 +170,27 @@ export async function insertGenresToMoviesByYear(year, page) {
                 .map(id => genresList.genres.filter(el => el.id === id))
                 .flat(),
         }));
-        markupMoviesByYear(fetchRes);
+        markupMoviesWithParams(fetchRes);
     }
     catch (error) {
         console.log(error);
     }
 }
 
-function markupMoviesByYear(year) {
-    year.map(element => {
+function markupMoviesWithParams(searchParam) {
+    searchParam.map(element => {
     if (element.genres.length > 2) {
-        const Obj = {name: "Other"};
+        const Obj = otherGenresTemplate();
         element.genres[2] = Obj;
         element.genres.length = 3
     }
     })
-        renderMoviesList(year);
+        renderMoviesList(searchParam);
 };
 
+function onSearchFailure() {
+  Report.failure(
+    'Search Failure',
+    'Sorry, there is no movie matched your query. Please try again.',
+    'Ok');
+};
